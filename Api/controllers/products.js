34dee251ff3e3ec86e3757471e1766d7b65a71dc
eduardo-felix <duduflj@gmail.com>
2024-09-03 11:13:53 -0300

    Routes and Controller implemented

--- Api/controllers/products.js
+++ Api/controllers/products.js
@@ -1,2 +1,17 @@
-import { db_conection } from "../server/db_conection";
+const { pool } = require('../server/db_conection');
 
+const getProducts = async (req, res) => {
+    try {
+        const registeredProducts = await pool.query('SELECT * FROM produtos');
+        if (!registeredProducts) {
+        return console.log("Não existem produtos cadastrados");
+        }     
+    return res.status(200).json(registeredProducts)    
+    } catch (error) {
+        res.status(500).json({ mensagem: error.message }) 
+    }  
+}
+
+module.exports = {
+    getProducts
+}
--- Api/index.js
+++ Api/index.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const cors = require("cors");
+const productRoute = require("./routes/productRoute.js");
 
 const app = express();
 
+app.use(productRoute);
+
 app.use(express.json());
 app.use(cors());
+
+
 
 
 app.listen(8000, () => console.log('Server Rodando na porta 8000'));
